0756c46c0f0614d6cc5c1432f8bdca6a2f1e1e99
bolonge <qhsghd5357@gmail.com>
2022-01-24 22:48:14 +0900

    post model

--- post/models.py
+++ post/models.py
@@ -1,3 +1,16 @@
+from distutils.command.upload import upload
 from django.db import models
+from user.models import UserModel
 
-# Create your models here.
+
+class PostModel(models.Model):
+
+    class Meta:
+        db_table = 'post'
+
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=256)
+    photo = models.ImageField('상품', upload_to='iterms', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
