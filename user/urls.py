502826a500ee55feefc4bbd087f959b8b76212e4
kidcode <tmdgus5611@gmail.com>
2022-01-25 15:35:46 +0900

    user

--- user/urls.py
+++ user/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('sign-in/', views.sign_in, name='sign-in'),
     path('sign-up/', views.sign_up, name='sign-up'),
-    # path('post/<int:id>', views.delete_post, name='delete-post'),
+    path('logout/', views.logout, name='logout'),
+
 ]
