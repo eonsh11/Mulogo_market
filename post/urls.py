c31630eddb0bb908aed729a1fb0875db9984fab5
bolonge <qhsghd5357@gmail.com>
2022-01-25 16:41:37 +0900

    create post

--- post/urls.py
+++ post/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('main/', views.main, name='main'),
-    path('create-post/', views.create_post, name='create_post')
+    path('create-post/', views.create_post, name='create-post')
     # path('post/<int:id>', views.delete_post, name='delete-post'),
 ]
--- post/views.py
+++ post/views.py
@@ -1,5 +1,6 @@
 from django.http import request
 from django.shortcuts import render, redirect
+from .models import PostModel
 
 
 def home(request):
@@ -18,5 +19,13 @@ def create_post(request):
     if request.method == "GET":
         return render(request, 'post/create-post.html')
     elif request.method == "POST":
-
+        user = request.user
+        title = request.POST.get('title', '')
+        content = request.POST.get('content', '')
+        photo = request.POST.get('url', '')
+        if content == '':
+            return render(request, 'post/create-post.html', {'error': '게시글은 공백일 수 없습니다'})
+        my_tweet = PostModel.objects.create(
+            user=user, title=title, photo=photo, content=content)
+        my_tweet.save()
         return redirect('/main')
